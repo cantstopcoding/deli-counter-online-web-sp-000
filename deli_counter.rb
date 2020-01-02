c5555ecfca90f3e9bc83d7fc08d962ac10e638c5
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 23:08:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-     i = 0 
-     while i < katz_deli.length  
+    i = 0 
+    while i < katz_deli.length 
+     
+    end
   end 
 end 
