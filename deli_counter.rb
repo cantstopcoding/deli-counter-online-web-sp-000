75c21946197e998764e6427974fa3318d6e4948a
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 23:04:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
+    index = 1 
     katz_deli.each do |name|
-      puts 
+      puts "The line is currently: #{index}. "
     end 
   end 
 end 
