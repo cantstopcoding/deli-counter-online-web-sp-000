9301148575be1cefe51f27508f37d14e462644e3
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 23:32:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0
-    i = 0
-    num_line = []
-    katz_deli.each do |name|
-      num_line << "#{katz_deli.index(name) + 1}. #{name}"
+    num_line = katz_deli.map do |name|
+      "#{katz_deli.index(name) + 1}. #{name}"
     end
     puts "The line is currently: #{num_line.join(' ')}"
   end 
